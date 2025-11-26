ee6d3f52c9a0c3101a3f1d0c67afa6264d361aef
Aditya Dixit <dixit99304@gmail.com>
2025-11-26 05:33:59 +0530

    feat: Added MongoDB-compatible User model and a web route to list users.

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,7 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+// use Illuminate\Foundation\Auth\User as Authenticatable;
+use MongoDB\Laravel\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
@@ -17,6 +18,9 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
+    protected $connection = 'mongodb';
+    protected $collection = 'users';
+
     protected $fillable = [
         'name',
         'email',
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\User;
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/users', function () {
+    $users = User::limit(5)->get(); 
+    return response()->json($users);
 });
