10585c2852d659e95f2a0ea837a633a6ae4a7595
Aditya Dixit <dixit99304@gmail.com>
2025-11-26 18:56:19 +0530

    Added Event repository pattern with interface.

---
+++ app/Repositories/EventRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Repositories\EventRepository;
+use App\Models\Event;
+
+class EventRepository implements EventRepositoryInterface{
+    public function all(){
+        return Event::all();
+    }
+    public function find($id){
+        return Event::find($id);
+    }
+    public function create(array $data){
+        return Event::create($data);
+    }
+    public function update(array $data, $id){
+        $event = Event::find($id);
+        $event->update($data);
+        return $event;
+    }
+    public function delete($id){
+        $event = Event::find($id);
+        $event->delete();
+    }
+}
